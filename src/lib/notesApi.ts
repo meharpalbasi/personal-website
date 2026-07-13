db0f64b50ef702cbf18d5522debbb348893b07f6
MeharpalBasi <meharpalbasi45@gmail.com>
2026-07-13 23:46:04 +0100

    refactor: enhance notesApi to safely access properties with optional chaining and default values

--- src/lib/notesApi.ts
+++ src/lib/notesApi.ts
@@ -152,17 +152,24 @@ class NotesApi {
             'multi_select' in page.properties.hashtags
               ? page.properties.hashtags.multi_select.map((tag) => tag.name)
               : [],
-          title: 'title' in page.properties.title ? page.properties.title.title[0].plain_text : '',
+          title:
+            'title' in page.properties.title
+              ? page.properties.title.title[0]?.plain_text ?? ''
+              : '',
           description:
             'rich_text' in page.properties.description
-              ? page.properties.description.rich_text[0].plain_text
+              ? page.properties.description.rich_text[0]?.plain_text ?? ''
               : '',
           slug:
-            'rich_text' in page.properties.slug ? page.properties.slug.rich_text[0].plain_text : '',
+            'rich_text' in page.properties.slug
+              ? page.properties.slug.rich_text[0]?.plain_text ?? ''
+              : '',
           isPublished:
             'checkbox' in page.properties.published ? page.properties.published.checkbox : false,
           publishedAt:
-            'date' in page.properties.publishedAt ? page.properties.publishedAt.date!.start : '',
+            'date' in page.properties.publishedAt
+              ? page.properties.publishedAt.date?.start ?? page.created_time
+              : page.created_time,
           inProgress:
             'checkbox' in page.properties.inProgress ? page.properties.inProgress.checkbox : false,
         };
